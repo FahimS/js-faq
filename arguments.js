1b357d72e13e2907771e0363735148cbe8343717
Fahim Shahriar <fahim.cse.uap@gmail.com>
2021-10-08 23:11:49 +0600

    added

--- arguments.js
+++ arguments.js
@@ -8,3 +8,24 @@ function addNumbers(num1, num2) { //num1, num2 use na korleo kono problem nai
 }
 var result = addNumbers(5, 4, 9, 7);
 console.log(result);
+
+
+ 
+//....................................
+
+function addNumbers(num1, num2) { //num1, num2 use na korleo kono problem nai
+    var sum = 0;
+    for (let i = 0; i < arguments.length; i++) {
+        const num = arguments[i];
+        sum = sum + num;
+    }
+    function logInfo(message) {
+        console.log(message);
+    }
+    logInfo('Good Morning');
+
+    var double = sum * 2;
+    return double;
+}
+var result = addNumbers(5, 4, 9, 7);
+console.log(result);
